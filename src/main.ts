77e5d46f546138f25ee24097939031fe4b1e0092
haider-centrox <haider.ali.centrox@gmail.com>
2026-02-06 15:38:29 +0500

    fix: doamin ssl

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const allowedOrigins = [
-    'http://ec2-3-15-229-133.us-east-2.compute.amazonaws.com:3000',
+    'https://www.quickflashcards.site',
+    'http://localhost:3000',
   ];
   app.enableCors({
     origin: allowedOrigins,
